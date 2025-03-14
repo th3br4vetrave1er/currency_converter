29da0853cfc19d62d9113e140de1b6891d4be675
th3br4vetrave1er <tazdingodan@gmail.com>
2025-03-14 19:27:51 +0200

    i hate rates >:(

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ def load_rates(json_file: str) -> dict[str, dict]:
         return json.load(file)
 
 
-def convert(
-    amount: float,
-    base: str, to: str,
-    rates: dict[str, dict]
-     ) -> float:
-    base: str = base.lower()
-    to: str = to.lower()
+def convert(amount: float, base: str, to: str, rates: dict[str, dict]) -> float:
+    base: str = input('What currency are we looking at: ').lower()
+    while base not in rates:
+        print('You must choose one of the rates: ')
+        print(rates.keys())
+        base: str = input('What currency are we looking at: ').lower()
 
-    from_rates: dict | None = rates.get(base)
-    to_rates: dict | None = rates.get(to)
+    to: str = input('To what currency are we converting: ').lower()
+    while to not in rates:
+        print('You must choose one of the rates: ')
+        print(rates.keys())
+        to: str = input('So whats the target currency? : ').lower()
+
+        from_rates: dict | None = rates.get(base)
+        to_rates: dict | None = rates.get(to)
 
     if base == 'eur':
         return amount * to_rates['rate']
@@ -25,7 +30,7 @@ def convert(
 
 def main() -> None:
     rates: dict[str, dict] = load_rates('rates.json')
-    result: float = convert(amount=10, base='lolo', to='dkk', rates=rates)
+    result: float = convert(amount=10, base='PAB', to='dkk', rates=rates)
     print(result)
 
 
